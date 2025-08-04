d660aba2b7e8bf42cc913bb1aaa0a0831c3526fe
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 12:29:34 +0000

    Add program to print single-digit numbers 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
