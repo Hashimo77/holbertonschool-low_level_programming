789d13765bbfecb502575472199ed7fb36b1ff1f
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 11:33:20 +0000

    0-positive_or_negative.c dosyası eklendi

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int n;
@@ -10,11 +15,17 @@ int main(void)
     n = rand() - RAND_MAX / 2;
 
     if (n > 0)
+    {
         printf("%d is positive\n", n);
+    }
     else if (n == 0)
+    {
         printf("%d is zero\n", n);
+    }
     else
+    {
         printf("%d is negative\n", n);
+    }
 
     return (0);
 }
