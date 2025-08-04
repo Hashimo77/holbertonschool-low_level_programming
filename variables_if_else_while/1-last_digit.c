5cd673bf737ca262659f134e62e0e5b0870c2143
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 12:06:30 +0000

    Fix Betty indent errors in 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a random number
- *
- * Return: Always 0 (Success)
- */
+	* main - Prints the last digit of a random number
+	*
+	* Return: Always 0 (Success)
+	*/
 int main(void)
 {
-    int n;
-    int last;
+	int n;
+	int last;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-    printf("Last digit of %d is %d and is ", n, last);
+	printf("Last digit of %d is %d and is ", n, last);
 
-    if (last > 5)
-        printf("greater than 5\n");
-    else if (last == 0)
-        printf("0\n");
-    else
-        printf("less than 6 and not 0\n");
+	if (last > 5)
+	printf("greater than 5\n");
+	else if (last == 0)
+	printf("0\n");
+	else
+	printf("less than 6 and not 0\n");
 
-    return (0);
+	return (0);
 }
