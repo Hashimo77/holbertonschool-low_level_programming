0f197d7628fa4d6770248fc1de5a1f5a9144c038
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 16:56:50 +0000

    Correct implementation of print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char c;
+    int count = 0;
+    char letter;
 
-    for (i = 0; i < 10; i++)
+    while (count++ < 10)
     {
-        c = 'a';
-        while (c <= 'z')
-        {
-            _putchar(c);
-            c++;
-        }
+        for (letter = 'a'; letter <= 'z'; letter++)
+            _putchar(letter);
         _putchar('\n');
     }
 }
