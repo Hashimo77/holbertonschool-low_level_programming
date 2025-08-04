c58e09e3e06bb8eddbaf59e159601ea6ee58e72e
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 17:32:42 +0000

    Fix _abs implementation with proper header inclusion

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the integer to check
- * Return: the absolute value of n
+ * _abs - computes absolute value of integer
+ * @n: integer to check
+ * Return: absolute value of n
  */
 int _abs(int n)
 {
-	return (n < 0 ? -n : n);
+	if (n < 0)
+		return (-n);
+	return (n);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,13 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int print_sign(int n);
-
-#endif
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
 int _abs(int n);
 
 #endif
