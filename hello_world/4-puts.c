219b542c90a392d52ffc57fdeac6ff56daa54d62
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 12:42:34 +0000

    Add program using puts to print specific string

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Description: Prints exactly
-* "Programming is like building a multilingual puzzle"
-* followed by a new line.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints a specific string using puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
