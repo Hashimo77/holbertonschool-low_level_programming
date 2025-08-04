28fff964c2fe8a91cc9fb9e071b0bdc2ff24053a
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 11:05:01 +0000

    Add 6-size.c: prints size of types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Description: Prints size of various types
+* on the computer it is compiled and run on.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
