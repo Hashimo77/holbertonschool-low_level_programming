64e7f131ffc7df60d38e06c1726d0e0c4c8fde45
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 16:59:13 +0000

    Fixed compilation issues in alphabet printer

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet
  */
 void print_alphabet_x10(void)
 {
-    int count = 0;
-    char letter;
+	int i;
+	char c;
 
-    while (count++ < 10)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-            _putchar(letter);
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
