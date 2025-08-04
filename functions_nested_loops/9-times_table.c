c5db10078b871d13bd382fdd4206c24886305306
Hashimo77 <hagamirz7@gmail.com>
2025-08-04 17:54:06 +0000

    Fix times_table implementation with proper formatting

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,12 +21,11 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar(', ');
+				_putchar(',\);
 				_putchar(' ');
 				if (product < 10)
 					_putchar(' ');
-				else
-					_putchar('0' + (product / 10));
+				_putchar(product / 10 ? '0' + (product / 10) : ' ');
 				_putchar('0' + (product % 10));
 			}
 		}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,13 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void);
-
-#endif
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
 void times_table(void);
 
 #endif
