07d5fda83d7d337bfe8c82391429e171c629b3a1
Your Name <your@email.com>
2025-08-05 12:50:43 +0000

    main_0.c dosyası güncellendi

--- functions_nested_loops/main_0.c
+++ functions_nested_loops/main_0.c
@@ -1,27 +1,11 @@
 #include "main.h"
-
-/**
- * test_isalpha - test the _isalpha function
- * Return: void
- */
-void test_isalpha(void)
-{
-	int r;
-	int n = 108; /* ASCII for 'l' */
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(n);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-}
+#include <stdio.h>
 
 int main(void)
 {
-	test_isalpha();
-	return (0);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
