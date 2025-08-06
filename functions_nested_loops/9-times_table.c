717e9579ed40de6dffb9bf235e5945a14f4b1211
Hashimo77 <hagamirz7@gmail.com>
2025-08-06 11:06:52 +0000

    Fix times_table function with proper _putchar declaration

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,27 +13,27 @@ void times_table(void)
 		{
 			prod = i * j;
 
-			if (j == 0)
-			{
-				_putchar(prod + '0');
-			}
-			else if (prod < 10)
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(prod + '0');
+				if (prod < 10)
+					_putchar(' ');
+			}
+
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				_putchar(prod + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
+
 
 
