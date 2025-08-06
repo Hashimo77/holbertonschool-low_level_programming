369a3cae73d64e48b85ad0a0ff21acc02e57a40b
Hashimo77 <hagamirz7@gmail.com>
2025-08-06 11:01:32 +0000

    Add times_table function with proper formatting

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes character to stdout
- * @c: character to print
- * Return: On success 1
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
  */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
+
