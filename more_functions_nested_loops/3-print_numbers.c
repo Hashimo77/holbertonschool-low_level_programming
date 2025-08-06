af5144dc56fc51b01fbd55482a7998b55902c347
Hashimo77 <hagamirz7@gmail.com>
2025-08-06 10:28:20 +0000

    Fix print_numbers indentation and remove leading spaces

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ *
+ * You can only use _putchar twice in your code
  */
 void print_numbers(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
+
