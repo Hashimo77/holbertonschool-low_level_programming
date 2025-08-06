9824fdcdfb7e48b3c053f252da57fa60bc8b16f9
Hashimo77 <hagamirz7@gmail.com>
2025-08-06 10:52:57 +0000

    Remove swap files and add them to gitignore

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-	int i, j, res;
+	int i, j, prod;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			res = i * j;
+			prod = i * j;
 			if (j == 0)
 			{
-				_putchar('0' + res);
+				_putchar(prod + '0');
 			}
-			else if (res < 10)
+			else if (prod < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + res);
+				_putchar(prod + '0');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar('0' + (res / 10));
-				_putchar('0' + (res % 10));
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
+O
+
