143a31f5f5307435729567cef5be3640a1708e97
Hashimo77 <hagamirz7@gmail.com>
2025-08-11 11:18:31 +0000

    Add _puts function implementation and prototype

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
