da5b2e333779aa4852d599ef87992000b67b69de
Hashimo77 <hagamirz7@gmail.com>
2025-08-11 11:35:48 +0000

    Add _atoi function to convert string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,45 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
- * Return: the integer value
+ * _atoi - convert string to integer
+ * @s: input string
+ *
+ * Return: integer value
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int started = 0;
-	int minus_count = 0;
+    unsigned int result = 0;
+    int sign = 1;
+    int started = 0;
 
-	while (*s)
-	{
-		if (*s == '-')
-			minus_count++;
-		else if (*s == '+')
-			; /* ignore plus signs */
-		else if (*s >= '0' && *s <= '9')
-		{
-			started = 1;
-			result = result * 10 + (*s - '0');
-		}
-		else if (started)
-			break;
-		s++;
-	}
+    while (*s)
+    {
+        if (*s == '-')
+            sign *= -1;
+        else if (*s == '+' && !started)
+            ;
+        else if (*s >= '0' && *s <= '9')
+        {
+            started = 1;
 
-	if (minus_count % 2 != 0)
-		sign = -1;
+            if (result > (unsigned int)(2147483647 / 10) ||
+                (result == (unsigned int)(2147483647 / 10) && (*s - '0') > 7))
+            {
+                /* overflow occurred */
+                if (sign == 1)
+                    return (2147483647);
+                else
+                    return (-2147483648);
+            }
 
-	return (sign * result);
+            result = result * 10 + (*s - '0');
+        }
+        else if (started)
+            break;
+        s++;
+    }
+
+    return (sign * (int)result);
 }
+
 
