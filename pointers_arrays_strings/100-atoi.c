0867bd50a128e33668bd45bb4a0ca0bcc51895ba
Hashimo77 <hagamirz7@gmail.com>
2025-08-13 10:24:10 +0000

    Add _strcmp implementation and prototype

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,43 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
  *
- * Return: the integer representation of the string
+ * Return: the integer value
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0, found_digit = 0;
+	int i = 0, sign = 1, num = 0, started = 0;
 
-	/* Skip all characters until first digit or sign */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* ignore plus signs */
+		{
+			/* ignore plus sign */
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			found_digit = 1;
-			break;
+			started = 1;
+
+			/* Overflow check */
+			if (sign == 1 && num > (INT_MAX - (s[i] - '0')) / 10)
+				return (INT_MAX);
+			if (sign == -1 && num > (INT_MAX - (s[i] - '0')) / 10)
+				return (INT_MIN);
+
+			num = num * 10 + (s[i] - '0');
 		}
+		else if (started)
+			break;
 		i++;
 	}
-
-	if (!found_digit)
-		return (0);
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	return (num * sign);
 }
+
 
 
 
