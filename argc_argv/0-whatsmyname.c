807886bef5f5c0a865377abea1ef232985ee9a57
Hashimo77 <hagamirz7@gmail.com>
2025-08-19 11:49:55 +0000

    Add 0-whatsmyname.c and README.md

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
  * main - prints the name of the program
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    (void)argc; /* argc istifadə olunmur, ona görə kompilyator xəbərdarlığı üçün */
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
+
 
