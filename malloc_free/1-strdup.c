a64a7c390f43929c4dd3be9db7732c4764d9d774
Hashimo77 <hagamirz7@gmail.com>
2025-08-20 15:25:54 +0000

    Task 1: Implement _strdup function

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated copy of a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL if str is NULL or malloc fails
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
