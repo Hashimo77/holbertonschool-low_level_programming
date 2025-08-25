268db6996338d334b1396173af0bd5c6277092a0
Hashimo77 <hagamirz7@gmail.com>
2025-08-25 16:40:20 +0000

    Add struct dog definition

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - it haqqında məlumat saxlayan struktur
+ * @name: itin adı
+ * @age: itin yaşı
+ * @owner: sahibinin adı
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
+
+
