91dac7869019945da4455c5fd21d2c7291fbc676
Hashimo77 <hagamirz7@gmail.com>
2025-08-25 17:00:56 +0000

    betty fixed

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,20 +1,30 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h> /* NULL tanımı için */
+
 /**
- * struct dog - it haqqında məlumat saxlayan struktur
- * @name: itin adı
- * @age: itin yaşı
+ * struct dog - köpek yapısı
+ * @name: köpeğin adı
+ * @age: köpeğin yaşı
  * @owner: sahibinin adı
+ *
+ * Açıklama: Bu struct bir köpeği temsil etmek için kullanılır.
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
+/* typedef ile struct dog için yeni isim dog_t oluşturuldu */
+typedef struct dog dog_t;
+
+/* Fonksiyon prototipleri */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* DOG_H */
-
-
-
