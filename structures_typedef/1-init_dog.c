3ec1ca83a5b932ebf333ccf9677e0aa31ccd545e
Hashimo77 <hagamirz7@gmail.com>
2025-08-25 18:14:41 +0000

    betty fixed

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,17 +1,19 @@
-#include "dog.h"   /* struct dog ve prototip için başlık dosya
-		     * sı dahil edildi
-		    */
-#include <stddef.h> /* NULL tanımı için *//**
- * init_dog - bir struct dog değişkenini başlatır
- * @d: başlatılacak struct dog işaretçisi
- * @name: köpeğin adı
- * @age: köpeğin yaşı
- * @owner: köpeğin sahibi
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL) /* Eğer gelen işaretçi NULL ise fonksiyondan çık */
-		return;	d->name = name;   /* Köpeğin adını struct içine ata */
-	d->age = age;     /* Köpeğin yaşını struct içine ata */
-	d->owner = owner; /* Köpeğin sahibini struct içine ata */
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
+
