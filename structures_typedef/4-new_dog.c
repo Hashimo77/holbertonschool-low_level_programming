a72c9778c9924b3e509bbaaabfeed29ff34f1dbd
Hashimo77 <hagamirz7@gmail.com>
2025-08-25 18:17:23 +0000

    fixed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,43 +1,67 @@
 #include <stdlib.h>
-#include "dog.h"/**
- * str_copy - Bir string'in kopyasını oluşturur
- * @str: Kopyalanacak string
+#include "dog.h"
+
+/**
+ * str_copy - creates a copy of a string
+ * @str: string to copy
  *
- * Return: Yeni string'in adresi veya NULL
+ * Return: pointer to the new string or NULL if failure
  */
 char *str_copy(char *str)
 {
 	char *copy;
-	int len = 0, i;	if (str == NULL)
-		return (NULL);	while (str[len] != '\0')
-		len++;	copy = malloc(len + 1);
+	int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	copy = malloc(len + 1);
 	if (copy == NULL)
-		return (NULL);	for (i = 0; i <= len; i++)
-		copy[i] = str[i];	return (copy);
-}/**
- * new_dog - Yeni bir dog_t oluşturur ve kopyalarını saklar
- * @name: Köpeğin adı
- * @age: Köpeğin yaşı
- * @owner: Köpeğin sahibi
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+
+/**
+ * new_dog - creates a new dog and stores copies of name and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * Return: Başarılıysa yeni dog_t işaretçisi, başarısızsa NULL
+ * Return: pointer to new dog_t, or NULL if failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;	d = malloc(sizeof(dog_t));
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-		return (NULL);	d->name = str_copy(name);
+		return (NULL);
+
+	d->name = str_copy(name);
 	if (name != NULL && d->name == NULL)
 	{
 		free(d);
 		return (NULL);
-	}	d->owner = str_copy(owner);
+	}
+
+	d->owner = str_copy(owner);
 	if (owner != NULL && d->owner == NULL)
 	{
 		if (d->name != NULL)
 			free(d->name);
 		free(d);
 		return (NULL);
-	}	d->age = age;
+	}
+
+	d->age = age;
+
 	return (d);
 }
+
