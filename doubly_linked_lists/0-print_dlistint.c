fd73b1234a24d88557c2075853948ce9c3ffb877
Hashimo77 <hagamirz7@gmail.com>
2025-09-09 09:33:44 +0000

    Add print_dlistint and lists.h

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
@@ -9,14 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
 
-    return (count);
+	return (nodes);
 }
