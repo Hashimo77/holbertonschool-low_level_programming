fdb69a510b0d16b2bcbaacab66c5f18968105f9a
Hashimo77 <hagamirz7@gmail.com>
2025-09-10 13:18:11 +0000

    Add read_textfile main.h and README.md

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,45 +6,45 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
+ * @filename: name of the file
  * @letters: number of letters to read and print
  *
  * Return: actual number of letters read and printed, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r, w;
-    char *buffer;
+	int fd;
+	ssize_t r, w;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 
-    r = read(fd, buffer, letters);
-    if (r == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    w = write(STDOUT_FILENO, buffer, r);
-    free(buffer);
-    close(fd);
+	w = write(STDOUT_FILENO, buffer, r);
+	free(buffer);
+	close(fd);
 
-    if (w != r)
-        return (0);
+	if (w != r)
+		return (0);
 
-    return (w);
+	return (w);
 }
