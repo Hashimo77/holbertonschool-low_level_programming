c62fda94d943fd0e542e028225e241efd3558823
Hashimo77 <hagamirz7@gmail.com>
2025-09-10 13:53:46 +0000

    Add cp.c and main.h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,26 +5,75 @@
 
 #define BUF_SIZE 1024
 
-void print_error_exit(int code, const char *msg, const char *file, int fd);
-int open_file_from(char *filename);
-int open_file_to(char *filename);
-void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
-void close_fd(int fd);
+/**
+ * error_exit - prints an error message and exits with a specific code
+ * @code: exit code
+ * @file: filename or fd (if needed)
+ */
+void error_exit(int code, const char *file, int fd)
+{
+	if (code == 97)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	else if (code == 98)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	else if (code == 99)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	else if (code == 100)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 
+	exit(code);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUF_SIZE];
 
 	if (ac != 3)
-		print_error_exit(97, NULL, NULL, 0);
+		error_exit(97, NULL, 0);
 
-	fd_from = open_file_from(av[1]);
-	fd_to = open_file_to(av[2]);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, av[1], 0);
 
-	copy_file(fd_from, fd_to, av[1], av[2]);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		error_exit(99, av[2], 0);
+	}
 
-	close_fd(fd_from);
-	close_fd(fd_to);
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, av[2], 0);
+		}
+	}
+
+	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		error_exit(98, av[1], 0);
+	}
+
+	if (close(fd_from) == -1)
+		error_exit(100, NULL, fd_from);
+
+	if (close(fd_to) == -1)
+		error_exit(100, NULL, fd_to);
 
 	return (0);
 }
